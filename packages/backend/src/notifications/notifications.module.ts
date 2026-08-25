633dc67fb2520d60187b460a1e4c10d566ddb824
Shlok007 <shlokpandey120@gmail.com>
2026-08-25 11:27:39 +0530

    fix: add AuthModule to NotificationsModule imports

--- packages/backend/src/notifications/notifications.module.ts
+++ packages/backend/src/notifications/notifications.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common'
 import { NotificationsController } from './notifications.controller'
 import { NotificationsService } from './notifications.service'
 import { PrismaService } from '../prisma.service'
+import { AuthModule } from '../auth/auth.module'
 
 @Module({
+  imports: [AuthModule],
   controllers: [NotificationsController],
   providers: [NotificationsService, PrismaService],
   exports: [NotificationsService],
